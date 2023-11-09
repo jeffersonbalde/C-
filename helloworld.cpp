2cc0722669deeb496f68038a3884a348604287b3
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-11-10 07:58:13 +0800

    hello world

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // std::cout << "Hello cplusplus" << std::endl;
+    std::cout << "Hello cplusplus" << '\n';
+    std::cout << "I am IAN";
+    // cout << "Hello again" << endl;
+    return 0;
+}
