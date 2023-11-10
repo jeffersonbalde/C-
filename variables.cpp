8651cf12b7023b8eb89b9a888cbd0bc4958fbc55
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-11-10 08:16:24 +0800

    variables

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main() {
+    int x = 13;
+    double temp = 10.23;
+    char name = '1';
+
+    std::cout << x << std::endl;
+    std::cout << temp << std::endl;
+    std::cout << name << std::endl;
+
+    bool isTrue = false;
+    std::cout << isTrue << std::endl;
+
+    std::string fullName = "Jefferson Balde";
+    std::cout << fullName;
+    return 0;
+}
